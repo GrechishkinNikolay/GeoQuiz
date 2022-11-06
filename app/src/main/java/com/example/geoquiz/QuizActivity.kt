30b099353f400a98cf841d968065e28ab25b6588
Grechishkin Nikolay <kolaygreha9@gmail.com>
2022-11-06 18:51:57 +0400

    3

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -1,11 +1,18 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        val mTrueButton = findViewById<TextView>(R.id.true_button)
+        val mFalseButton = findViewById<TextView>(R.id.false_button)
+
+        mTrueButton.setOnClickListener { TODO("Not yet implemented") }
     }
 }
