e2f77d0d8edff9cb926f2a6d9efb724b80fd4cfa
Grechishkin Nikolay <kolaygreha9@gmail.com>
2022-11-14 18:50:21 +0400

    add views lazy initialization and more questions

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -7,19 +7,45 @@ import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity : AppCompatActivity() {
 
+    private val mQuestionTextView: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.question_text_view) }
+    private val mTrueButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.true_button) }
+    private val mFalseButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.false_button) }
+    private val mNextButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.next_button) }
+
+    private val mQuestionBank = arrayOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var mCurrentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
-        val mTrueButton = findViewById<TextView>(R.id.true_button)
-        val mFalseButton = findViewById<TextView>(R.id.false_button)
+        val question: Int = mQuestionBank[mCurrentIndex].textResId
+        mQuestionTextView.setText(question)
 
         mTrueButton.setOnClickListener {
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
         }
 
         mFalseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
+
+        mNextButton.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+            val question = mQuestionBank[mCurrentIndex].textResId
+            mQuestionTextView.setText(question)
+        }
+    }
+
+    private fun updateQuestion() {
+        val question = mQuestionBank[mCurrentIndex].textResId
+        mQuestionTextView.setText(question)
     }
 }
