ec423b05b1fb0ffdd920181a88f104bce96fa2c7
Grechishkin Nikolay <kolaygreha9@gmail.com>
2022-11-16 20:29:04 +0400

    saving configuration in bundle and landscape layout

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -1,16 +1,22 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity : AppCompatActivity() {
 
+    private val TAG = "QuizActivity345"
+    private val KEY_INDEX = "questionCurrentIndex"
+
     private val mQuestionTextView: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.question_text_view) }
     private val mTrueButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.true_button) }
     private val mFalseButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.false_button) }
-    private val mNextButton: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.next_button) }
+    private val mNextButton: ImageButton by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.next_button) }
+    private val mPrevButton: ImageButton by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.previous_button) }
 
     private val mQuestionBank = arrayOf(
         Question(R.string.question_australia, true),
@@ -22,10 +28,19 @@ class QuizActivity : AppCompatActivity() {
     )
     private var mCurrentIndex = 0
 
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
+        mCurrentIndex = savedInstanceState?.getInt(KEY_INDEX) ?: 0
+
+        Log.d(TAG, "onCreate(savedInstanceState: Bundle?")
         updateQuestion()
 
         mTrueButton.setOnClickListener {
@@ -40,6 +55,42 @@ class QuizActivity : AppCompatActivity() {
             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
             updateQuestion()
         }
+
+        mPrevButton.setOnClickListener {
+            mCurrentIndex =
+                if ((mCurrentIndex - 1) >= 0) (mCurrentIndex - 1) % mQuestionBank.size else mQuestionBank.size - 1
+            updateQuestion()
+        }
+
+        mQuestionTextView.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+            updateQuestion()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
