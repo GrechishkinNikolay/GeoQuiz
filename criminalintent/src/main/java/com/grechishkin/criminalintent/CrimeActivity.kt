1e30a0e2e1031bef45182977aef210b69f8c3c77
Grechishkin Nikolay <kolaygreha9@gmail.com>
2022-12-01 17:49:43 +0400

    add CriminalIntent app

---
+++ criminalintent/src/main/java/com/grechishkin/criminalintent/CrimeActivity.kt
@@ -0,0 +1,11 @@
+package com.grechishkin.criminalintent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CrimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crime)
+    }
+}
